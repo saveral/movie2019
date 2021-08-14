01da3d1dd297a9cb417f3de552df90a3a0a55c21
saveral <saveral@naver.com>
2021-08-15 02:59:59 +0900

    추가파일

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import About from "./routes/About"
+import Home from "./routes/Home"
+import Navigation from "./components/Navigation";
+import "./App.css";
+import Detail from "./routes/Detail";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    return(
+     <HashRouter> {/* 네비게이션의 link를 사용하려면 무조건 라우터 안에 있어야 된다 */}
+         <Navigation />
+         <Route path="/" exact={true} component={Home} />
+         <Route path="/about" component={About} />
+         <Route path="/movie-detail" component={Detail} />
+    </HashRouter>
+    );
 }
 
 export default App;
